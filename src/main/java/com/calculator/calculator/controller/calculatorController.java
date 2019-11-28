7044b3c9c6ccee6716de2d440828d58b101c1407
Alex Trask <Sarlack_Work@mail.ru>
2019-11-28 12:40:57 +0300

    Basic REST is done

--- src/main/java/com/calculator/calculator/controller/calculatorController.java
+++ src/main/java/com/calculator/calculator/controller/calculatorController.java
@@ -1,8 +1,7 @@
 package com.calculator.calculator.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.calculator.calculator.exeptions.NotFoundExeption;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -13,14 +12,53 @@ import java.util.Map;
 @RequestMapping("counter")
 public class calculatorController {
 
-    public List<Map<String, String>> counters = new ArrayList<Map<String, String>>() {{
+    private List<Map<String, String>> counters = new ArrayList<Map<String, String>>() {{
         add(new HashMap<String , String>() {{ put("name", "counter1"); put("value", "1"); }} );
         add(new HashMap<String , String>() {{ put("name", "counter2"); put("value", "1"); }} );
-        add(new HashMap<String , String>() {{ put("name", "counter2"); put("value", "1"); }} );
+        add(new HashMap<String , String>() {{ put("name", "counter3"); put("value", "1"); }} );
     }};
 
     @GetMapping
     public List<Map<String, String>> counter() {
         return counters;
     }
+
+    @GetMapping("{name}")
+    public Map<String, String> getCounter(@PathVariable String name) {
+        return getCounterByName(name);
+    }
+
+    @PostMapping
+    public Map<String, String> create(@RequestBody Map<String, String> counter) {
+        counter.put("name", "counter" + (counters.size() + 1));
+
+        counters.add(counter);
+
+        return counter;
+    }
+
+    @PutMapping("{name}")
+    public Map<String, String> update(@PathVariable String name, @RequestBody Map<String, String> counter) {
+        Map<String, String> counterFromMemory = getCounterByName(name);
+
+        counterFromMemory.putAll(counter);
+        counterFromMemory.put("name", name);
+
+        return counterFromMemory;
+    }
+
+    @DeleteMapping("{name}")
+    public void delete(@PathVariable String name) {
+        Map<String, String> counter = getCounterByName(name);
+
+        counters.remove(counter);
+    }
+
+    private Map<String, String> getCounterByName(@PathVariable String name) {
+        return counters.stream().filter(counter -> counter.get("name")
+            .equals(name))
+            .findFirst()
+                .orElseThrow(NotFoundExeption::new);
+    }
+
 }
